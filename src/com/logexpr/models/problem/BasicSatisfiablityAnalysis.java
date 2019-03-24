2a79a58aa20f6c5d278cf24b1ef8e5c9c50022b2
Piotr Grabowski <avelanarius@gmail.com>
2019-03-24 20:07:25 +0100

    Full english UI localization.

--- src/com/logexpr/models/problem/BasicSatisfiablityAnalysis.java
+++ src/com/logexpr/models/problem/BasicSatisfiablityAnalysis.java
@@ -66,24 +66,24 @@ public class BasicSatisfiablityAnalysis {
 
     public String getDescriptiveStatus() {
         if (falseOutput.isSatisfiable() && trueOutput.isSatisfiable()) {
-            return "<html>&nbsp;&nbsp;Wyrażenie przyjmuje wartości <i><u><font color=red>fałszywe</font></u></i> i <i><u><font color=#00d600>prawdziwe</font></u></i>.</html>";
+            return "<html>&nbsp;&nbsp;Condition can be <i><u><font color=red>false</font></u></i> or <i><u><font color=#00d600>true</font></u></i>.</html>";
         } else if (falseOutput.isSatisfiable() && trueOutput.isUnsatisfiable()) {
-            return "<html>&nbsp;&nbsp;Wyrażenie logiczne jest <i><u><font color=red>zawsze fałszywe</font></u></i>.</html>";
+            return "<html>&nbsp;&nbsp;Condition is <i><u><font color=red>always false</font></u></i>.</html>";
         } else if (falseOutput.isUnsatisfiable() && trueOutput.isSatisfiable()) {
-            return "<html>&nbsp;&nbsp;Wyrażenie logiczne jest <i><u><font color=#00d600>zawsze prawdziwe</font></u></i>.</html>";
+            return "<html>&nbsp;&nbsp;Condition is <i><u><font color=#00d600>always true</font></u></i>.</html>";
         } else if (falseOutput.isUnsatisfiable() && trueOutput.isUnsatisfiable()) {
-            return "<html>&nbsp;&nbsp;Dziedzina jest <i><u>pusta</u></i>!</html>";
+            return "<html>&nbsp;&nbsp;Domain is <i><u>empty</u></i>!</html>";
         } else if (falseOutput.isSatisfiable() && trueOutput.isUnknown()) {
-            return "<html>&nbsp;&nbsp;Wyrażenie przyjmuje wartości <i><u><font color=red>fałszywe</font></u></i>, <br>&nbsp;&nbsp;lecz <i><u>nie wiadomo</u></i> czy przyjmuje wartość prawdziwą.</html>";
+            return "<html>&nbsp;&nbsp;Condition can be <i><u><font color=red>false</font></u></i>, <br>&nbsp;&nbsp;but <i><u>it is unknown</u></i> if it can be true.</html>";
         } else if (falseOutput.isUnknown() && trueOutput.isSatisfiable()) {
-            return "<html>&nbsp;&nbsp;Wyrażenie przyjmuje wartości <i><u><font color=#00d600>prawdziwe</font></u></i>, <br>&nbsp;&nbsp;lecz <i><u>nie wiadomo</u></i> czy przyjmuje wartość fałszywą.</html>";
+            return "<html>&nbsp;&nbsp;Condition can be <i><u><font color=#00d600>true</font></u></i>, <br>&nbsp;&nbsp;but <i><u>it is unknown</u></i> if it can be false.</html>";
         } else if (falseOutput.isUnknown() && trueOutput.isUnknown()) {
-            return "<html>&nbsp;&nbsp;<i><u>Nie wiadomo</u></i> jakie wartości logiczne przyjmuje wyrażenie.</html>";
+            return "<html>&nbsp;&nbsp;Condition logical values are <i><u>unknown</u></i>.</html>";
         } else if (falseOutput.isUnsatisfiable() && trueOutput.isUnknown()) {
-            return "<html>&nbsp;&nbsp;Wyrażenie nie przyjmuje wartości <i><u><font color=red>fałszywych</font></u></i>, <br>&nbsp;&nbsp;lecz <i><u>nie wiadomo</u></i> czy przyjmuje wartość prawdziwą.</html>";
+            return "<html>&nbsp;&nbsp;Condition cannot be <i><u><font color=red>false</font></u></i>, <br>&nbsp;&nbsp;but <i><u>it is unknown</u></i> if it can be true.</html>";
         } else if (falseOutput.isUnknown() && trueOutput.isUnsatisfiable()) {
-            return "<html>&nbsp;&nbsp;Wyrażenie nie przyjmuje wartości <i><u><font color=#00d600>prawdziwych</font></u></i>, <br>&nbsp;&nbsp;lecz <i><u>nie wiadomo</u></i> czy przyjmuje wartość fałszywą.</html>";
+            return "<html>&nbsp;&nbsp;Condition cannot be <i><u><font color=#00d600>true</font></u></i>, <br>&nbsp;&nbsp;but <i><u>it is unknown</u></i> if it can be false.</html>";
         }
-        return "<html><i><u>Nie wiadomo</u></i> jakie wartości logiczne przyjmuje wyrażenie.</html>";
+        return "<html>Condition logical values are <i><u>unknown</u></i>.</html>";
     }
 }
